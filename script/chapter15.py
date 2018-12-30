d02525054ac57ce2c6d2cdc98320df863e63cf28
y.taniguchi <y.softvalley@gmail.com>
2018-12-30 15:51:34 +0900

    chapter15

--- script/chapter15.py
+++ script/chapter15.py
@@ -1,3 +1,31 @@
-class point(object):
+import math
+import copy
+
+class Point(object):
     """Represents a point in 2-D space."""
+
+def distance_between_points(p1, p2):
+    return math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
+
+class Rectangle(object):
+    """Representes a rectangle
+
+    attributes: width, height, corner
+    """
+
+def move_rectangle(rect, dx, dy):
+    rect_cp = copy.deepcopy(rect)
+    rect_cp.corner.x += dx
+    rect_cp.corner.y += dy
+    return rect_cp
+
+
+class Circle(object):
+    """
+    attributes: center -> Point
+               ,radius -> float
+               """
+
+
+def point_in_circle(cir, p):
     
